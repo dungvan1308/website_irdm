1eba67cd941463d24bb6448027c196c76d747c78
Nguyen Van Dung <dungvan1308@gmail.com>
2026-08-09 21:20:10 +0700

    commit

--- apps/expert/tests.py
+++ apps/expert/tests.py
@@ -1,1 +1,27 @@
-from django.test import TestCase
+from django.test import TestCase, override_settings
+from django.urls import reverse
+
+from .models import Expert
+
+
+@override_settings(DEBUG=True)
+class ExpertDetailErrorPageTests(TestCase):
+	def test_unpublished_expert_uses_friendly_404_page(self):
+		expert = Expert.objects.create(
+			name="Chuyên gia bản nháp",
+			slug="chuyen-gia-ban-nhap",
+			is_active=True,
+			is_published=False,
+		)
+
+		response = self.client.get(
+			reverse("expert:detail", kwargs={"slug": expert.slug})
+		)
+
+		self.assertEqual(response.status_code, 404)
+		self.assertTemplateUsed(response, "errors/404.html")
+		self.assertContains(
+			response,
+			"Nội dung hiện chưa khả dụng",
+			status_code=404,
+		)
